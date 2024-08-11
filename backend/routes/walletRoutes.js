c3760f78be431e5acc2f7f62c142d1b0cbe1d932
axatbhardwaj <axatbhardwaj@outlook.com>
2024-08-11 22:27:07 +0530

    added middleware

--- backend/routes/walletRoutes.js
+++ backend/routes/walletRoutes.js
@@ -1,19 +1,19 @@
 const express = require('express');
-
+const validateMnemonicPhrase = require('../middlewares/validateMnemonicPhrase');
 const walletController = require('../controllers/walletController');
 const router = express.Router();
 
 //get requests 
 router.get("api/v1/wallet", walletController.createWallet);
-router.get("/api/v1/wallets/:mnemonicPhrase", walletController.getWallet);
+router.get("/api/v1/wallets/:mnemonicPhrase", validateMnemonicPhrase, walletController.getWallet);
 router.get("/api/v1/wallet/:address/balance", walletController.getBalance);
 
 
 // post requests
-router.post("/api/v1/wallet/import:mnemonicPhrase", walletController.importWallet);
+router.post("/api/v1/wallet/import:mnemonicPhrase", validateMnemonicPhrase, walletController.importWallet);
 router.post("/api/v1/wallet/addaccount/:index", walletController.addAccount);
 
 //delete requests
-router.delete("/api/v1/wallet/:mnemonicPhrase", walletController.deleteWallet);
+router.delete("/api/v1/wallet/:mnemonicPhrase", validateMnemonicPhrase, walletController.deleteWallet);
 
 module.exports = router;
